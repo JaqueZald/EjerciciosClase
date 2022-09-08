334145fe764a0feba223e94d303ccfea08f6cdcb
JaqueZald <93237978+JaqueZald@users.noreply.github.com>
2022-09-08 08:11:45 -0500

    080922

---
+++ src/ManejoErrores/Principal.java
@@ -0,0 +1,10 @@
+package ManejoErrores;
+
+public class Principal {
+    public static void main(String[] args) {
+        int[]numeros= {1, 2, 3};
+        System.out.print("Valor: ");
+        System.out.println(numeros[0]);
+
+    }
+}
