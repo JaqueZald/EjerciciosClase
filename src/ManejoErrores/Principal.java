3d6d10b37d04286a870080de1e78e84451154d7d
JaqueZald <93237978+JaqueZald@users.noreply.github.com>
2022-09-08 08:25:59 -0500

    080922

--- src/ManejoErrores/Principal.java
+++ src/ManejoErrores/Principal.java
@@ -2,9 +2,16 @@ package ManejoErrores;
 
 public class Principal {
     public static void main(String[] args) {
-        int[]numeros= {1, 2, 3};
+        int[] numeros = {1, 2, 3};
         System.out.print("Valor: ");
-        System.out.println(numeros[0]);
+        try {//Cuando es suceptible, evalua lo que hay entre los corchetes
+            System.out.println(numeros[3]);
+        } catch (Exception e) {//captura el error en el objeto e
+            System.out.println("Error en el codigo");
 
+        }
+
+        System.out.println("Final");
     }
 }
+
