5c3298fce7a38c342cf4c9633a0f0d12367bcae4
JaqueZald <93237978+JaqueZald@users.noreply.github.com>
2022-09-07 22:34:35 -0500

    070922

--- src/Figuras/Prueba.java
+++ src/Figuras/Prueba.java
@@ -1,8 +1,11 @@
 package Figuras;
 
+import java.util.Scanner;
+
 public class Prueba {
     public static void main(String[] args) {
-        Triangulo t1 = new Triangulo(2,3,4);
+        Scanner entrada = new Scanner(System.in);
+        /*Triangulo t1 = new Triangulo(2,3,4);
         //Figura f1 = new Figura();
         System.out.println(t1.toString());
         Cuadrado c1=new Cuadrado(2);
@@ -12,12 +15,74 @@ public class Prueba {
         t2.toString();
         Figura c2=new Cuadrado(3);
         c2.toString();
+*/
 
-
-
+        Figura[] figs = new Figura[3];
+        int opcion;
+        do {
+            menu();
+            opcion = entrada.nextInt();
+            entrada.nextLine();
+            switch (opcion) {
+                case 1:
+                    int opcionF;
+                    double lado;
+                    System.out.print("Ingresa el lado: ");
+                    lado = entrada.nextDouble();
+                    figs[0] = new Cuadrado(lado);
+                    System.out.println("Selecciona uno: ");
+                    System.out.println("1 Area: ");
+                    System.out.println("2 Perimetro: ");
+                    opcionF = entrada.nextInt();
+                    if (opcionF == 1) {
+                        System.out.println(figs[0].calcularArea());
+                    }
+                    if (opcionF == 2) {
+                        System.out.println(figs[0].calcularPerimentro());
+                    }
+                    break;
+                case 2:
+                    System.out.println("ingresa los lados");
+                    double lado1, lado2, lado3;
+                    System.out.print("Lado 1: ");
+                    lado1 = entrada.nextDouble();
+                    System.out.print("Lado 2: ");
+                    lado2 = entrada.nextDouble();
+                    System.out.print("Lado 3: ");
+                    lado3 = entrada.nextDouble();
+                    figs[1] = new Triangulo(lado1, lado2, lado3);
+                    System.out.println("Selecciona uno: ");
+                    System.out.println("1 Area: ");
+                    System.out.println("2 Perimetro: ");
+                    opcionF = entrada.nextInt();
+                    if (opcionF == 1) {
+                        System.out.println(figs[1].calcularArea());
+                    }
+                    if (opcionF == 2) {
+                        System.out.println(figs[1].calcularPerimentro());
+                    }
+                    break;
+                case 3:
+                    System.out.println("Salir");
+                    break;
+                default:
+                    System.out.println("No valido");
+                    break;
+            }
+        } while (opcion != 3);
     }
 
-}//tarea ir agregando el ejercicio de herencia, en ell de abstract
+    private static void menu() {
+        System.out.println();
+        System.out.print("Elige una figura: \n");
+        System.out.println("1- Cuadrado");
+        System.out.println("2 Triangulo");
+        System.out.println("3- Salir");
+        System.out.print("Escoje una opción: ");
+    }
+}
+
+//tarea ir agregando el ejercicio de herencia, en ell de abstract
 //añador cuadradolado
 //crear menu
 /*
---
+++ src/Simbologia.java
@@ -0,0 +1,22 @@
+public class Simbologia {
+    /*
+    (\)
+    ! not
+    printf("Tu promedio es %.1f \n"= salto de renglon
+    final double/int darle valor exacto a la variable
+    == Es igual
+    != Es distinto
+    <, <=, >, >= Menor, menor o igual, mayor, mayor o igual
+    &&= Operador and (y)
+    ||= Operador or (o)
+    != Operador not (no
+    % o mod= Resto de una división entre enteros
+    Math.sqrt (#) =sacar raices
+    Math.pow(#,#)= elevar a cualquier numero
+    Math.max(#, #)= maximo entre 2 numeros
+    ("variable" + "variable" ? " "texto" " : " "texto "));//if-else//se usa para sustituir esto
+    System.out.println("Ingresa la letra: ");
+      char letra=entrada.next().charAt(0);//asi se declaran letras
+    Character.isUpperCase// ver si una letra es mayuscula
+     */
+}
